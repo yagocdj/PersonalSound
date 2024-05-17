0be2bfcb7f607de0df96758447e9c2e941793a39
Yago César <yagocdj@gmail.com>
2024-05-16 23:02:40 -0300

    creating the home component's first block

--- personal-sound/src/app/app.routes.ts
+++ personal-sound/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
-import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
